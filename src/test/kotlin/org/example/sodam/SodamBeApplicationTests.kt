84b5674efbb64f70992f866de6fc8b00c4c08f8f
soohyeon <meltapplee@gmail.com>
2024-10-29 20:52:54 +0900

    setting

---
+++ src/test/kotlin/org/example/sodam/SodamBeApplicationTests.kt
@@ -0,0 +1,12 @@
+package org.example.sodam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SodamBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
