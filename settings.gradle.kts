2981969eefc1f66569d9c44c9c1aa47ebae4a7e5
soohyeon <meltapplee@gmail.com>
2024-10-29 21:25:47 +0900

    fixed untracked files

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "SODAM_BE"
--- src/main/kotlin/org/example/sodam/domain/user/domain/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.example.sodam.domain.user.domain
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun existsByAccountId(accountId: String): Boolean
-    fun findByAccountId(accountId: String): User?
-}
