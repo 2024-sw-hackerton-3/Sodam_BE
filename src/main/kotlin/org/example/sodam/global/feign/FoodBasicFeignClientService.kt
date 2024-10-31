6bbe9ccb8969589fd6b83e4a6a14eca1259451a2
soohyeon <meltapplee@gmail.com>
2024-10-31 18:38:13 +0900

    ktlint

--- src/main/kotlin/org/example/sodam/global/feign/FoodBasicFeignClientService.kt
+++ src/main/kotlin/org/example/sodam/global/feign/FoodBasicFeignClientService.kt
@@ -1,10 +1,10 @@
 package org.example.sodam.global.feign
 
+import org.example.sodam.domain.food.domain.BasicFood
+import org.example.sodam.domain.food.domain.BasicFoodRepository
 import org.example.sodam.global.feign.dto.response.BasicFoodResponse
 import org.example.sodam.global.s3.FileUtil
 import org.springframework.stereotype.Component
-import org.example.sodam.domain.food.domain.BasicFood
-import org.example.sodam.domain.food.domain.BasicFoodRepository
 
 @Component
 class FoodBasicFeignClientService(
@@ -16,7 +16,7 @@ class FoodBasicFeignClientService(
     fun getFoodInfo(name: String?): List<BasicFoodResponse> {
 //        val start = Random.nextInt(1, 138)
 //        val end = minOf(start + 400, 537)
-        val foods = if(name != null) basicFoodRepository.findAllByKoNameContains(name) else basicFoodRepository.findAll()
+        val foods = if (name != null) basicFoodRepository.findAllByKoNameContains(name) else basicFoodRepository.findAll()
 
 //        val foodBasicInfo = foodFeignClient.basicInfo(
 //            start = start.toString(),
@@ -32,7 +32,7 @@ class FoodBasicFeignClientService(
     private fun addToList(foodBasicListInfoJson: List<BasicFood>): List<BasicFoodResponse> {
 //        val content = foodBasicListInfoJson.Grid_20150827000000000226_1.row ?: emptyList()
 
-        return  foodBasicListInfoJson.map {  food ->
+        return foodBasicListInfoJson.map { food ->
             val image = when (food.koName) {
                 "비빔밥" -> fileUtil.generateObjectUrl("비빔밥.webp")
                 else -> fileUtil.generateObjectUrl("기본.png")
